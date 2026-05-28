449b8b6ddf51697823f120f34a97ece11f3bf286
loling2 <informatica@apedeca.es>
2026-05-28 14:01:25 +0100

    Updated supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseAnonKey = 'sb_publishable_Wjo15bYzoH2XR8OkNOpVEQ_hX3smnRN';
+/* const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string; */
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 

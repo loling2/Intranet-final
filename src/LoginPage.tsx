4e3d1cbf54708aab1a1ef1ba4805e5ccaa96f111
loling2 <informatica@apedeca.es>
2026-06-16 10:42:18 +0100

    Updated LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -6,7 +6,7 @@ import type { AppRole } from './supabaseClient';
 type UserRole = AppRole;
 import DocumentsCard from './DocumentsCard';
 import DevicesCard from './DevicesCard';
-import VehicleCard from './VehicleCard';
+import VehicleCard from './Vehiclecard';
 import CertificatesCard from './CertificatesCard';
 import ExamsCard from './ExamsCard';
 import PrevencionDocsCard from './PrevencionDocsCard';

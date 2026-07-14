358d0019352cd41ec317b8c8ffab3711e9d07bf5
loling222188-design <loling222.188@gmail.com>
2026-07-14 13:05:49 +0100

    Updated LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -2076,7 +2076,7 @@ useEffect(() => {
 
         {activeTab === 'calidad' && (
           <div className="grid gap-6">
-            <CalidadDocsCard theme={theme} societyId={session?.societyId ?? theme.id} />
+            <CalidadDocsCard theme={theme} societyId={theme.id} />
           </div>
         )}
 
